5a5239f96ba0a087d3da9a94510f051a0fda1d6a
KarolAz22 <karolina.azevedo@ufvjm.edu.br>
2022-10-31 15:15:42 -0300

    Modificando a forma de edição de registros

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -18,24 +18,8 @@
 
         <div class="iformacao-pagina">
             <div style="width:30%; margin-left:auto; margin-right:auto;">
-                <form method="post" action="">
-                    @csrf
-                    <input type="text" name="nome" value="" placeholder="Nome" class="borda-preta">
-
-                    <input type="text" name="descricao" value="" placeholder="Descrição" class="borda-preta">
-                   
-                    <input type="text" name="peso" value="" placeholder="Peso" class="borda-preta">
-                    
-                    <select name="unidade_id">
-                        <option>-- Selecione a unidade de medida --</option>
-                        @foreach ($unidades as $unidade)
-                            <option value="{{$unidade->id}}">{{$unidade->descricao}}</option>
-                            
-                        @endforeach
-                    </select>
-
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @component('app.produto._components.form_create_edit',  ['unidades' => $unidades])
+                @endcomponent
             </div>
         </div>
     </div>
--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -1,30 +1,32 @@
 @extends('app.layouts.basico')
 
 @section('titulo', 'Produto')
+
 @section('conteudo')
+
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
-            <p>Listagem de produtos</p>
+            <p>Listagem de Produtos</p>
         </div>
 
         <div class="menu">
             <ul>
-                <li><a href="{{route('produto.create')}}">Novo</a></li>
+                <li><a href="{{ route('produto.create') }}">Novo</a></li>
                 <li><a href="">Consulta</a></li>
-
             </ul>
         </div>
 
-        <div class="iformacao-pagina">
-            <div style="width:90%; margin-left:auto; margin-right:auto;">
-                <table border="1" width="1250" >
+        <div class="informacao-pagina">
+            <div style="width: 90%; margin-left: auto; margin-right: auto;">
+                <table border="1" width="100%">
                     <thead>
                         <tr>
                             <th>Nome</th>
                             <th>Descrição</th>
                             <th>Peso</th>
                             <th>Unidade ID</th>
+                            <th></th>
                             <th></th>
                             <th></th>
                         </tr>
@@ -37,18 +39,39 @@
                                 <td>{{ $produto->descricao }}</td>
                                 <td>{{ $produto->peso }}</td>
                                 <td>{{ $produto->unidade_id }}</td>
-                                <td><a href="">Excluir</a></td>
-                                <td><a href="">Editar</a></td>
-
+                                <td><a href="{{ route('produto.show', ['produto' => $produto->id ]) }}">Visualizar</a></td>
+                                <td>
+                                    <form id="form_{{$produto->id}}" method="post" action="{{route('produto.destroy', ['produto'=>$produto->id])}}">
+                                        @method('DELETE')
+                                        @csrf
+                                        <!--<button type="submit">Excluir</button>-->
+                                        <a href="#" onclick="document.getElementById('form_{{$produto->id}}').submit()">Excluir</a>
+                                    </form>
+                                </td>
+                                <td><a href="{{ route('produto.edit', ['produto' =>$produto->id ]) }}">Editar</a></td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
 
-                {{$produtos->appends($request)->links()}}
+                {{ $produtos->appends($request)->links() }}
+
+                <!--
                 <br>
-                Exibindo {{$produtos->count()}} produtos de {{$produtos->total()}} ({{$produtos->firstItem()}} a {{$produtos->lastItem()}})
+                {{ $produtos->count() }} - Total de registros por página
+                <br>
+                {{ $produtos->total() }} - Total de registros da consulta
+                <br>
+                {{ $produtos->firstItem() }} - Número do primeiro registro da página
+                <br>
+                {{ $produtos->lastItem() }} - Número do último registro da página
+
+                -->
+                <br>
+                Exibindo {{ $produtos->count() }} produtos de {{ $produtos->total() }} (de {{ $produtos->firstItem() }} a {{ $produtos->lastItem() }})
             </div>
         </div>
+
     </div>
+
 @endsection
